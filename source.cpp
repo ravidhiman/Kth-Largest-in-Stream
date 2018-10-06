7425662a091d1aa9d0d6dbf1d533518d1c0d4b26
Ravinder Dhiman <dhiman.88@gmail.com>
2018-10-06 20:20:51 +0530

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+struct func
+{
+	bool operator()(int a, int b)
+	{
+		return a > b;
+	}
+};
+
+int main()
+{
+	vector<int> vec = { 10, 20, 11, 70, 50, 40, 100, 5 };
+	priority_queue<int,vector<int>,func> pq;
+	int k = 3;
+
+	int i = 0;
+
+	while (i < k && i<vec.size())
+	{
+		pq.push(vec[i]);
+		i++;
+	}
+	if (i == k)
+		cout << pq.top() << endl;
+
+	while(i < vec.size())
+	{
+		if (vec[i] > pq.top())
+		{
+			pq.pop();
+			pq.push(vec[i]);
+		}
+		cout << pq.top() << " ";
+		i++;
+	}
+	return 0;
+}
